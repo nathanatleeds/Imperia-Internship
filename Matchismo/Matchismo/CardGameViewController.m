11a86e488d61102fdc42616d664450706df31482
slaviyana chervenkondeva <slaviyana.chervenkondeva@Trainings-Mac-mini.local>
2018-07-05 13:40:18 +0300

    Completed random card flip task

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,14 +7,28 @@
 //
 
 #import "CardGameViewController.h"
+#import "Card.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (nonatomic, strong) Deck *deck;
 
 @end
 
 @implementation CardGameViewController
+
+-(Deck *) deck
+{
+    if(!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+-(Deck *) createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
 
 - (void)setFlipCount:(int)flipCount
 {
@@ -30,16 +44,21 @@
         UIImage *cardImage = [UIImage imageNamed:@"cardback"];
         [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+         self.flipCount++;
     }
     
     else
     {
-        UIImage *cardImage = [UIImage imageNamed:@"cardfront"];
-        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *card = [self.deck drawRandomCard];
+        if(card)
+        {
+            UIImage *cardImage = [UIImage imageNamed:@"cardfront"];
+            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+            [sender setTitle:card.contents forState:UIControlStateNormal];
+            self.flipCount++;
+        }
+
     }
-    
-    self.flipCount++;
 }
 
 @end
