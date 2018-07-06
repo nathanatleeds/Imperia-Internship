a6ae97568d7ee9ba3cbda7bd5ba37a8570a06fdb
slaviyana chervenkondeva <slaviyana.chervenkondeva@Trainings-Mac-mini.local>
2018-07-06 13:36:17 +0300

    Created the Attributor for the Color Switch app

---
+++ Attributor/Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by slaviyana chervenkondeva on 6.07.18.
+//  Copyright © 2018 slaviyana chervenkondeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
