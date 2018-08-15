12cc094056fac45fa62b2d5d9e00f1c59b19a1c8
slaviyana chervenkondeva <slaviyana.chervenkondeva@Trainings-Mac-mini.local>
2018-08-15 12:21:14 +0300

    Finished android tic tac toe

--- Android/TicTacToe2/app/src/main/java/com/slaviyanachervenkondeva/tictactoe2/MainActivity.java
+++ Android/TicTacToe2/app/src/main/java/com/slaviyanachervenkondeva/tictactoe2/MainActivity.java
@@ -3,12 +3,18 @@ package com.slaviyanachervenkondeva.tictactoe2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // 0 = x, 1 = o, 2 = empty
     int activePlayer = 0;
+
+    boolean gameIsActive = true;
+
     int[] gameState= {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3,4,5}, {6,7,8}, {0,3,6},
             {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
@@ -19,12 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void playAgain(View view) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+
+        gameIsActive = true;
+
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        GridLayout board;
+        board = (GridLayout) findViewById(R.id.boardLayout);
+
+        for(int i = 0; i < board.getChildCount(); i++) {
+            ((ImageView)board.getChildAt(i)).setImageResource(0);
+        }
+    }
+
     public void draw(View view) {
         ImageView counter = (ImageView) view;
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if(gameState[tappedCounter] == 2) {
+        if(gameState[tappedCounter] == 2 && gameIsActive) {
 
             gameState[tappedCounter] = activePlayer;
             counter.setTranslationY(-1000f);
@@ -44,7 +71,45 @@ public class MainActivity extends AppCompatActivity {
                 if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2) {
-                    System.out.println(gameState[winningPosition[0]]);
+                    //System.out.println(gameState[winningPosition[0]]);
+
+                    //Someone has won
+                    gameIsActive = false;
+
+                    String winner = "O";
+                    if(gameState[winningPosition[0]] == 0) {
+                        winner = "X";
+                    }
+
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+
+                    winnerMessage.setText(winner + " has one!");
+
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+
+                    layout.setVisibility(View.VISIBLE);
+
+                } else {
+                    boolean gameIsOver = true;
+
+                    for(int counterState : gameState) {
+                        if (counterState == 2) {
+                            gameIsOver = false;
+                        }
+                    }
+                    if(gameIsOver) {
+
+                            TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+                            winnerMessage.setText("Draw");
+
+                            LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+
+                            layout.setVisibility(View.VISIBLE);
+                    }
+
                 }
             }
         }
